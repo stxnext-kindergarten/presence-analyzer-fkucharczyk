ed932a9fa19cd4fadf3f0d7d52724903aa0417ff
Pacu2 <filip.m.kucharczyk@gmail.com>
2016-12-06 15:40:36 +0100

    Update views.py

--- src/presence_analyzer/views.py
+++ src/presence_analyzer/views.py
@@ -5,7 +5,7 @@ import calendar
 from flask import redirect, abort
 
 from presence_analyzer.main import app
-from utils import jsonify, get_data, mean, group_by_weekday
+from presence_analyzer.utils import jsonify, get_data, mean, group_by_weekday
 
 import logging
 log = logging.getLogger(__name__)  # pylint: disable=invalid-name
